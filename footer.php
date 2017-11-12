8669c679ce72ab2c45264a2b64b073ff99bca288
Matt Price <matt.price@utoronto.ca>
2017-11-12 15:27:50 -0500

    fix broken browsersync issue
    Instead of trying to deal with the broken wordpreaa/browser--sync
    relationship, just add the browser-sync snippet manually to
    `footer.php`; this eliminates redirects that were plaguing me
    previously.

--- footer.php
+++ footer.php
@@ -15,44 +15,46 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="wrapper" id="wrapper-footer">
 
-	<div class="<?php echo esc_attr( $container ); ?>">
+    <div class="<?php echo esc_attr( $container ); ?>">
 
-		<div class="row">
+	<div class="row">
 
-			<div class="col-md-12">
+	    <div class="col-md-12">
 
-				<footer class="site-footer" id="colophon">
+		<footer class="site-footer" id="colophon">
 
-					<div class="site-info">
+		    <div class="site-info">
 
-							<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
-							/* translators:*/
-							esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
-								<span class="sep"> | </span>
-					
-							<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
-				
-							(<?php printf( // WPCS: XSS ok.
-							/* translators:*/
-								esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
-					</div><!-- .site-info -->
+			<a href="<?php  echo esc_url( __( 'http://wordpress.org/','understrap' ) ); ?>"><?php printf( 
+							                                                /* translators:*/
+							                                                esc_html__( 'Proudly powered by %s', 'understrap' ),'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			
+			<?php printf( // WPCS: XSS ok.
+			    /* translators:*/
+			    esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ), $the_theme->get( 'Name' ),  '<a href="'.esc_url( __('http://understrap.com', 'understrap')).'">understrap.com</a>' ); ?> 
+			
+			(<?php printf( // WPCS: XSS ok.
+			    /* translators:*/
+			    esc_html__( 'Version: %1$s', 'understrap' ), $the_theme->get( 'Version' ) ); ?>)
+		    </div><!-- .site-info -->
 
-				</footer><!-- #colophon -->
+		</footer><!-- #colophon -->
 
-			</div><!--col end -->
+	    </div><!--col end -->
 
-		</div><!-- row end -->
+	</div><!-- row end -->
 
-	</div><!-- container end -->
+    </div><!-- container end -->
 
 </div><!-- wrapper end -->
 
 </div><!-- #page we need this extra closing tag here -->
 
 <?php wp_footer(); ?>
-
+<script id="__bs_script__">//<![CDATA[
+ document.write("<script async src='http://HOST:3000/browser-sync/browser-sync-client.js?v=2.18.13'><\/script>".replace("HOST", location.hostname));
+ //]]></script>
 </body>
 
 </html>
