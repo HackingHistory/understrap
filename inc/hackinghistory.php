71fd96aeeae41346adc7a4da04afce2f70363704
Matt Price <matt.price@utoronto.ca>
2018-03-08 08:53:22 -0500

    replace js syntax with proper php in understrap_show_cards (!)

--- inc/hackinghistory.php
+++ inc/hackinghistory.php
@@ -11,7 +11,7 @@
 
 if ( ! function_exists( 'understrap_show_cards' ) ) :
 /**
- * Returns a series of posts as a set of Bootstrap cards. 
+ * Returns a series of posts as a set of Bootstrap cards.
  */
 function understrap_show_cards() {
     /**
@@ -30,22 +30,22 @@ function understrap_show_cards() {
     $the_query = new WP_Query( $args );
     /* a variable to hold the output */
     $html_out = "";
-    $card_open = '    <div class="card">' + "\n";
-    $card_close = '    </div>' + "\n";
-    
+    $card_open = '    <div class="card">' . "\n";
+    $card_close = '    </div>' . "\n";
+
     if ($the_query->have_posts() ) {
-        $html_out += '<div class="card-deck">';
+        $html_out .= '<div class="card-deck">';
         while ($the_query->have_posts() ) {
             $the_query->the_post();
-            $html_out += $card_open;
-            $html_out += "      " .  get_the_post_thumbnail(null, 'medium', ['class' => 'card-img-top']);
-            $html_out += '     <div class="card-body">' + "\n         " . '<h5 class="card-title">' ;
-            $html_out += get_the_title() . "/</h5>\n";
-            $html_out += '<p class="card-test">' . get_the_extract() . "</p>\n     </div>";
-            $html_out += $card_close;
+            $html_out .= $card_open;
+            $html_out .= "      " .  get_the_post_thumbnail(null, 'medium', ['class' => 'card-img-top']);
+            $html_out .= '     <div class="card-body">' . "\n         " . '<h5 class="card-title">' ;
+            $html_out .= get_the_title() . "/</h5>\n";
+            $html_out .= '<p class="card-test">' . get_the_excerpt() . "</p>\n     </div>";
+            $html_out .= $card_close;
         }
-        $html_out += "</div>\n";
+        $html_out .= "</div>\n";
     }
     echo $html_out;
-}              
+}
 endif;
