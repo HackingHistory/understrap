25c75cc4e19e272b3b890525a6fb760df8401950
Matt Price <matt.price@utoronto.ca>
2018-03-27 09:48:10 -0400

    start aside shortcode.  return instead of echo make_cards
    - get ready to shortcodeize things.

--- inc/hackinghistory.php
+++ inc/hackinghistory.php
@@ -1,19 +1,19 @@
 <?php
-/**
- * Custom template tags for this theme.
- *
- * Eventually, some of the functionality here could be replaced by core features.
- *
- * @package understrap
- */
+  /**
+   * Custom template tags for this theme.
+   *
+   * Eventually, some of the functionality here could be replaced by core features.
+   *
+   * @package understrap
+   */
 
 
 
-if ( ! function_exists( 'understrap_show_cards' ) ) :
-/**
- * Returns a series of posts as a set of Bootstrap cards.
- */
-function understrap_show_cards() {
+  if ( ! function_exists( 'understrap_show_cards' ) ) :
+  /**
+   * Returns a series of posts as a set of Bootstrap cards.
+   */
+  function understrap_show_cards() {
     /**
      *  Note that the function version of this code is somewhat more complex
      *  because we have to assemble the html as a string and then echo it,
@@ -22,9 +22,9 @@ function understrap_show_cards() {
 
     /* set up the query */
     $args = array(
-        'post_type' => 'page',
-        'orderby'   => 'rand',
-        'posts_per_page' => 3,
+      'post_type' => 'page',
+      'orderby'   => 'rand',
+      'posts_per_page' => 3,
     );
 
     $the_query = new WP_Query( $args );
@@ -34,18 +34,62 @@ function understrap_show_cards() {
     $card_close = '    </div>' . "\n";
 
     if ($the_query->have_posts() ) {
-        $html_out .= '<div class="card-deck">';
-        while ($the_query->have_posts() ) {
-            $the_query->the_post();
-            $html_out .= $card_open;
-            $html_out .= "      " . get_the_post_thumbnail(null, 'medium', ['class' => 'card-img']);
-            $html_out .= '     <div class="card-img-overlay">' . "\n         " . '<h5 class="card-title">' ;
-            $html_out .= get_the_title() . "</h5>\n";
-            $html_out .= '<p class="card-test">' . wp_trim_words(get_the_excerpt(), "15") . "</p>\n     </div>";
-            $html_out .= $card_close;
-        }
-        $html_out .= "</div>\n";
+      $html_out .= '<div class="card-deck">';
+      while ($the_query->have_posts() ) {
+        $the_query->the_post();
+        $html_out .= $card_open;
+        $html_out .= "      " . get_the_post_thumbnail(null, 'medium', ['class' => 'card-img']);
+        $html_out .= '     <div class="card-img-overlay">' . "\n         " . '<h5 class="card-title">' ;
+        $html_out .= get_the_title() . "</h5>\n";
+        $html_out .= '<p class="card-test">' . wp_trim_words(get_the_excerpt(), "15") . "</p>\n     </div>";
+        $html_out .= $card_close;
+      }
+      $html_out .= "</div>\n";
     }
-    echo $html_out;
-}
-endif;
+   return  $html_out;
+  }
+  endif;
+
+  /* add "showcards" as a shortcode  */
+  add_shortcode( 'showcards', 'understrap_show_cards' );
+
+  if ( ! function_exists( 'understrap_make_aside' ) ) :
+  /**
+   * Returns a series of posts as a set of Bootstrap cards.
+   */
+  function understrap_make_aside(content) {
+    /**
+     *  Note that the function version of this code is somewhat more complex
+     *  because we have to assemble the html as a string and then echo it,
+     * rather than using the `the_title` type functions.
+     */
+
+    /* set up the query */
+    $args = array(
+      'post_type' => 'page',
+      'orderby'   => 'rand',
+      'posts_per_page' => 3,
+    );
+
+    $the_query = new WP_Query( $args );
+    /* a variable to hold the output */
+    $html_out = "";
+    $card_open = '    <div class="card">' . "\n";
+    $card_close = '    </div>' . "\n";
+
+    if ($the_query->have_posts() ) {
+      $html_out .= '<div class="card-deck">';
+      while ($the_query->have_posts() ) {
+        $the_query->the_post();
+        $html_out .= $card_open;
+        $html_out .= "      " . get_the_post_thumbnail(null, 'medium', ['class' => 'card-img']);
+        $html_out .= '     <div class="card-img-overlay">' . "\n         " . '<h5 class="card-title">' ;
+        $html_out .= get_the_title() . "</h5>\n";
+        $html_out .= '<p class="card-test">' . wp_trim_words(get_the_excerpt(), "15") . "</p>\n     </div>";
+        $html_out .= $card_close;
+      }
+      $html_out .= "</div>\n";
+    }
+    return $html_out;
+  }
+  endif;
